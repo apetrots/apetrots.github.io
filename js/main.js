3482c7ed59da914339e7e0550c1c42eba83183c5
Zach <zachary.newsom1@gmail.com>
2023-04-19 20:25:25 -0700

    Test3

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ async function loadArticlesAndIndex(tags, articleCount)
         appendToIndex(row);
     }
 
-    stmt.step();
+    // stmt.step();
     // get the rest of the rows
     while(stmt.step()) {
         const row = stmt.getAsObject();
